6c592726296bb4378909d8be2afb5e5f89ec3442
jreitz2 <jreitz2@gmail.com>
2023-09-25 10:06:28 -0400

    changed auth to google

--- src/App.js
+++ src/App.js
@@ -1,54 +1,55 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { Routes, Route, useNavigate } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import Header from './Components/Header';
-import Home from './Components/Home';
-import SignIn from './Components/SignIn';
-import CreateAccount from './Components/CreateAccount';
-
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
 
 function App() {
-
   const firebaseConfig = {
     apiKey: "AIzaSyBSf5_GLw4PvLihhymyqic9ygs3OWuRn24",
     authDomain: "just-chat-496c6.firebaseapp.com",
     projectId: "just-chat-496c6",
     storageBucket: "just-chat-496c6.appspot.com",
     messagingSenderId: "645952286564",
-    appId: "1:645952286564:web:7d35976b48b96015eb1471"
+    appId: "1:645952286564:web:7d35976b48b96015eb1471",
   };
 
   const app = initializeApp(firebaseConfig);
+  const provider = new GoogleAuthProvider();
+  provider.setCustomParameters({ prompt: "select_account" });
   const auth = getAuth();
   const user = auth.currentUser;
   const db = getFirestore();
- 
-  const [ isLoggedIn, setIsLoggedIn ] = useState(false);
-  const navigate = useNavigate();
 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
     onAuthStateChanged(auth, (data) => {
-      if(data) {
+      if (data) {
         setIsLoggedIn(true);
-        navigate('/');
+        console.log("User signed in:", user);
       } else {
         setIsLoggedIn(false);
       }
-    })
-  }, [])
-
+    });
+  }, [auth, user]);
 
   return (
     <div className="App">
-      <Header isLoggedIn={isLoggedIn} auth={auth} user={user} />
-        <Routes>
-          <Route path='/' element={<Home db={db} user={user} isLoggedIn={isLoggedIn} />}></Route>
-          <Route path='/signin' element={<SignIn auth={auth} />}></Route>
-          <Route path='/createaccount' element={<CreateAccount auth={auth} />}></Route>
-        </Routes>
+      <Header
+        isLoggedIn={isLoggedIn}
+        auth={auth}
+        user={user}
+        provider={provider}
+      />
+      <Routes>
+        <Route
+          path="/"
+          element={<Home db={db} user={user} isLoggedIn={isLoggedIn} />}
+        ></Route>
+      </Routes>
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,33 +1,45 @@
 import { Link } from "react-router-dom";
-import { signOut } from "firebase/auth";
+import { signOut, signInWithRedirect } from "firebase/auth";
 
+const Header = ({ isLoggedIn, auth, user, provider }) => {
+  const handleSignIn = () => {
+    signInWithRedirect(auth, provider)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-const Header = ( {isLoggedIn, auth, user } ) => {
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        console.log("logged out");
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
 
-    const handleLogout = () => {
-        signOut(auth)
-            .then(() => {
-                console.log('logged out');
-            })
-            .catch((err) => {
-                console.log(err.message)
-            })
-    }
+  return (
+    <div className="header">
+      <Link to="/" className="logo">
+        Just Chat!
+      </Link>
+      <div className="account-buttons">
+        {!isLoggedIn && <button onClick={handleSignIn}>Sign-in</button>}
+        {isLoggedIn && (
+          <div className="account-signed-in">
+            <img src={user.photoURL} alt="user" />
+            {user.displayName}
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
-    return ( 
-        <div className="header">
-            <Link to='/' className="logo">Just Chat!</Link>
-            <div className="account-buttons">
-                {!isLoggedIn && <Link to='/signin'><button>Sign-in</button></Link>}
-                {!isLoggedIn && <Link to='/createaccount'><button>Create account</button></Link>}
-                {isLoggedIn && 
-                    <div>
-                        {user.email} <button onClick={handleLogout}>Logout</button>
-                    </div>}
-            </div>
-            
-        </div>
-     );
-}
- 
 export default Header;
